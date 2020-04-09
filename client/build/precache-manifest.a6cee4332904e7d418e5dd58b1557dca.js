69461ef502542c406ccd1d74d260bdb7f15a4cc6
Erin Commarato <ecommarato@healthgrades.com>
2020-04-08 22:11:02 -0600

    Added client build

---
+++ client/build/precache-manifest.a6cee4332904e7d418e5dd58b1557dca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d909390790f67d80ab",
+    "url": "/static/css/main.8d797ac6.chunk.css"
+  },
+  {
+    "revision": "98d909390790f67d80ab",
+    "url": "/static/js/main.cf15f7d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04e94b5c3521cba60a00",
+    "url": "/static/js/2.3b453603.chunk.js"
+  },
+  {
+    "revision": "c7dba5488e6cd214233baa168d1b2a6c",
+    "url": "/index.html"
+  }
+];
